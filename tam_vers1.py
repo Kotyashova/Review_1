f034664068654f85162afe1bbece660d05c2cf73
Kotyashova <73757604+Kotyashova@users.noreply.github.com>
2020-11-14 19:41:30 +0300

    Update tam_vers1.py

--- tam_vers1.py
+++ tam_vers1.py
@@ -27,7 +27,6 @@ class Pet(object):
         print('Omnomnom')
         self.hunger -= food
         self.hunger = max(0, self.hunger)
-        delta = time.time() - time_0
 
     def play(self, fun=15):
         print('Netflix and chill.')
@@ -40,36 +39,34 @@ class Pet(object):
         self.boredom = max(0, self.boredom)
 
     def period(self, delta):
-        self.hunger += round((delta / 10), 1)
-        self.boredom += round((delta / 10), 1)
+        self.hunger += round(delta / 10)
+        self.boredom += round(delta / 10)
+
 
 tam_name = input('What will you name your pet?\n')
 tam = Pet(tam_name)
 time_0 = time.time()
 choice = None
 while choice != '0':
-    print('0 - Exit\n1 - Find out the mood\n2 - Feed\n3 - Play\n4 - Learn')
+    print('0 - Exit', '1 - Find out the mood', '2 - Feed', '3 - Play', '4 - Learn', sep='\n')
     print('Choose command:\n')
-    choice = int(input())
+    choice = input()
     delta = time.time() - time_0
     Pet.period(tam, delta)
-    if choice == 0:
+    if choice == '0':
         print('Good luck!')
-    elif choice == 1:
+        break
+    elif choice == '1':
         Pet.talk(tam)
-        time_0 = time.time()
-    elif choice == 2:
+    elif choice == '2':
         Pet.eat(tam)
-        time_0 = time.time()
-    elif choice == 3:
+    elif choice == '3':
         Pet.play(tam)
-        time_0 = time.time()
-    elif choice == 4:
+    elif choice == '4':
         Pet.learn(tam)
-        time_0 = time.time()
     else:
         print('Invalid input. Choose another command.')
-        time_0 = time.time()
+    time_0 = time.time()
     if tam.hunger >= 100 or tam.boredom >= 100:
         print('Your pet is dead.')
         break
